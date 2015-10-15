b989743feaf8e2a81cfc07fa64c9fef579791503
Tomas López Fragoso <tlopezfr@ull.edu.es>
2015-10-15 10:51:39 +0100

    Adding prompt for file selection

--- perfcsvplot.py
+++ perfcsvplot.py
@@ -5,8 +5,12 @@ import numpy
 import csv #For working with CSV
 import sys
 
+#Select the file:
+print "Select the file to open (with path and extension)?"
+data = raw_input("> ")
+
 #Open the CSV:
-with open('/home/tomas/Escritorio/Doctorado/Programas/python/A1000.csv', 'rb') as csvfile:
+with open(data, 'rb') as csvfile:
 	query = csv.reader(csvfile, delimiter=',')
 	for row in query:
 		print ', '.join(row)
