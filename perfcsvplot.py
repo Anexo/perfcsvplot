ab5838568a0a0dcdecbeae1c2231e5e4da1e8d28
Tomas López Fragoso <tlopezfr@ull.edu.es>
2015-10-15 11:35:52 +0100

    Plotting example, adding title and labels

--- perfcsvplot.py
+++ perfcsvplot.py
@@ -1,13 +1,18 @@
 # Basic script to import CSv files exported from Perf tool and plotting them.    
 #------------------------------------------------------------------------------
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 import csv #For working with CSV
 import sys
 
 #Select the file:
 print "Select the file to open (with path and extension)?"
 data = raw_input("> ")
+#Select the title and ylabel:
+print "Select the title of the plot"
+title = raw_input("> ")
+print "Select the Y label of the plot"
+ylabel = raw_input("> ")
 
 #Open the CSV:
 with open(data, 'rb') as csvfile:
@@ -15,10 +20,16 @@ with open(data, 'rb') as csvfile:
 	for row in query:
 		print ', '.join(row)
 
-'''
-print "Plot:" 
-plt.show()
+#Ejemplo con el seno: 
+x = np.linspace(0, 10)
+line, = plt.plot(x, np.sin(x), '-', linewidth=2)
 
-print "Done!"
+#Print plot:
+plt.title(title)
+plt.xlabel('Time (s)')
+plt.ylabel(ylabel)
+plt.show()
+print "Plot finished. Bye!"
+
+#Exit script
 sys.exit()
-'''
