206be5e7a34f92391d8f0d302ca7c0f9c14551e4
Tomas López Fragoso <tlopezfr@ull.edu.es>
2015-10-16 09:00:50 +0100

    Fix encoding bug

--- perfcsvplot.py
+++ perfcsvplot.py
@@ -1,18 +1,17 @@
 # Basic script to import CSV files exported from Perf tool and plotting them.    
 #------------------------------------------------------------------------------
-#
-#Copyright (C) 2015 by Tomás López-Fragoso Rumeu [Github](https://github.com/Anexo)
-#
+
+#Copyright (C) 2015 by Tomas Lopez-Fragoso Rumeu <https://github.com/Anexo>
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
 #the Free Software Foundation, either version 3 of the License, or
 #(at your option) any later version.
-#
+
 #This program is distributed in the hope that it will be useful,
 #but WITHOUT ANY WARRANTY; without even the implied warranty of
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #GNU General Public License for more details.
-#
+
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>
 #------------------------------------------
